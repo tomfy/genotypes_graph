2c60aab74f1261599a9d04bcb1056c33d054eeb3
Thomas Fisher-York <tly2@cornell.edu>
2019-08-20 16:58:28 -0400

    genotype -> sequence add generation, mean method

--- Genotype.pm
+++ Genotype.pm
@@ -4,7 +4,7 @@ use namespace::autoclean;
 use Carp;
 use List::Util qw ( min max sum );
 
-use constant MISSING_DATA => 3;
+use constant MISSING_DATA => '-';
 #Readonly my  $BIG_NUMBER => 1_000_000_000;
 
 # a class for genotypes
@@ -19,11 +19,17 @@ has id => (
            required => 1,
           );
 
-has genotype => (
+has sequence => (
                  isa => 'ArrayRef', # e.g. [0,1,1,1,0,2,1,1,2,3,1,1,0,1,0,3,2];
                  is => 'ro',
                  required => 1,
                 );
+
+has generation => (
+                   isa => 'Int',
+                   is => 'ro',
+                   default => sub { undef },
+                   );
 
 has pedigree => (
                  isa => 'Str',
@@ -38,7 +44,7 @@ around BUILDARGS => sub {
    if (@_ == 1  and  (!ref $_[0]) ) { # argument should be string with >id generation pedigree \n sequence.
       my $arg = $_[0];
     #  print "XXXXX: $arg \n";
-      my ($idline, $sequence) = split(/\n/, $arg);
+      my ($idline, $sequence_string) = split(/\n/, $arg);
       my ($id, $generation, $pedigree) = (undef, undef, undef);
       if ($idline =~ /^>(\S+)\s+(\d+)\s+(\S+)\s*$/x) {
          ($id, $generation, $pedigree) = ($1, $2, $3);
@@ -47,9 +53,9 @@ around BUILDARGS => sub {
       } else {
          die "in Genotype BUILDARGS argument $arg does not have expected >id format.\n";
       }
-      $sequence =~ s/\s+//gx;
-      my @sequence_array = split(//, $sequence);
-      return {id => $id, genotype => \@sequence_array, pedigree => $pedigree};
+      $sequence_string =~ s/\s+//gx;
+      my @sequence_array = split(//, $sequence_string);
+      return {id => $id, sequence => \@sequence_array, generation => $generation, pedigree => $pedigree};
    }
    # otherwise, don't modify arguments, should be array ref with id, genotype, and optionally pedigree
 };
@@ -58,8 +64,8 @@ around BUILDARGS => sub {
 sub distance{ # calculate distance between this genotype obj. and another
    my $self = shift;
    my $other_genotype = shift;
-   my $this_gt = $self->genotype();            # array ref of 0,1,2,3
-   my $other_gt = $other_genotype->genotype(); # array ref of 0,1,2,3
+   my $this_gt = $self->sequence();            # array ref of 0,1,2,3
+   my $other_gt = $other_genotype->sequence(); # array ref of 0,1,2,3
 
    my $distance = 0;
    my $count_both = 0; # count of snps with data present in both sequences
@@ -67,7 +73,7 @@ sub distance{ # calculate distance between this genotype obj. and another
    if (scalar @$this_gt == scalar @$other_gt) {
       while (my($i, $c1) = each @{$this_gt}) {
          my $c2 = $other_gt->[$i];
-         if ( ($c1 == MISSING_DATA)  or ($c2 == MISSING_DATA) ) {
+         if ( ($c1 eq MISSING_DATA)  or ($c2 eq MISSING_DATA) ) {
             $count_missing++;
          } else {
             $count_both++;
@@ -88,6 +94,26 @@ sub distance{ # calculate distance between this genotype obj. and another
    return ($distance, $count_both, $count_missing);
 }
 
+sub mean{
+   my $self = shift;
+   my $other_genotype = shift;
+   my $this_gt = $self->sequence();         # array ref of 0,1,2,3
+   my $other_gt = $other_genotype->sequence(); # array ref of 0,1,2,3
+   my @mean_gt = ();
+   if (scalar @$this_gt == scalar @$other_gt) { # check that lengths are equal
+      while (my($i, $c1) = each @{$this_gt}) {
+         my $c2 = $other_gt->[$i];
+         if ($c1 eq MISSING_DATA) {
+            push @mean_gt, $c2;
+         } elsif ($c2 eq MISSING_DATA) {
+            push @mean_gt, $c1;
+         } else {
+            push @mean_gt, 0.5*($c1 + $c2);
+         }
+      }
+   }
+   return \@mean_gt;
+}
 
 ############################################
 
