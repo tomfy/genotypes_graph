f17935f21994461d96e06835717b0d368abddf20
Thomas Fisher-York <tly2@cornell.edu>
2019-08-22 15:25:35 -0400

    look for modules GenotypeTree.pm, etc. in ../lib rel. to where this script is locate.

--- bin/gtree_from_fasta.pl
+++ bin/gtree_from_fasta.pl
@@ -3,30 +3,29 @@ use strict;
 use warnings;
 use Readonly;
 use List::Util qw (min max sum);
-use GenotypeTree;
-use GenotypeTreeNode;
-use Genotype;
 use Getopt::Long;
 use lib '/home/tomfy/Orthologger/lib/';
 use TomfyMisc qw ' fasta2seqon1line ';
 
+use File::Basename 'dirname';
+use Cwd 'abs_path';
+my ( $bindir, $libdir );
+BEGIN {     # this has to go in Begin block so happens at compile time
+   $bindir =
+     dirname( abs_path(__FILE__) ) ; # the directory containing this script
+   $libdir = $bindir . '/../lib';
+   $libdir = abs_path($libdir); # collapses the bin/../lib to just lib
+}
+use lib $libdir;
+
+use GenotypeTree;
+use GenotypeTreeNode;
+use Genotype;
+
 {                               ###########
-
-   #  my $n_nearest_to_keep =  5; # for each genotype make this many directed edges in graph, to the $n_nearest_to_keep closest other nodes.
-
    my $input_filename = undef;  # input fasta file name.
- 
-   # my $output_distance_matrix = 1; # whether to output a distance matrix ( .dmatrix filename ending)
-   # my $output_graph = 1; # whether to output the graph (.grph filename ending0
-   # my $multiplier = 10000; # controls # significant digits. 10000 -> 0.6492361... is output as 6492
-   # my $show_sequence = 0; # if true, will output the sequence at the end of line.#
    GetOptions(
               'input_filename=s' => \$input_filename,
-              # 'nearest=i' => \$n_nearest_to_keep, # e.g. '*.newick'
-              # 'distance_matrix_out!' => \$output_distance_matrix,
-              # 'graph_out!' => \$output_graph,
-              # 'multiplier=i' => \$multiplier,
-              # 'sequence_out!' => \$show_sequence,
              );
 
    my $input_filename_stem = $input_filename;
@@ -86,80 +85,6 @@ use TomfyMisc qw ' fasta2seqon1line ';
       $gtree->add_genotype($gobj);
    }
 
-   print "\n\n[", $gtree->as_string(), "]\n";
+   print "\n\n[", $gtree->as_string(1), "]\n";
 
 }                               # end main
-
-################ subroutines ############################
-
-# find all the distances and store in a hash.
-# keys are node ids, and values are hash refs of node id : distance pairs;
-# i.e. $idA__idB_distance{$idA}->{$idB} is the distance between nodes with ids $idA and $idB
-# sub store_distances{
-#    my $id_seq = shift;
-
-#    my @ids = keys %$id_seq;
-
-#    my %idA__idB_distance = ();  # hash of hash refs
-#    while (my ($i1, $id1) = each @ids) {
-#       #   print STDERR "$i1 $id1 \n";
-#       my $s1 = $id_seq->{$id1};
-#       for (my $i2 = $i1+1; $i2 < scalar @ids; $i2++) {
-#          my $id2 = $ids[$i2];
-#          #    print STDERR "    $i2 $id2 \n";
-#          my $s2 = $id_seq->{$id2};
-#          my ($d_ij, $both_snp_count) = distance($s1, $s2);
-#          #   print "$id1  $id2  $d_ij \n";
-#          my $d = ($both_snp_count > 0)? $d_ij/$both_snp_count : BIG_NUMBER; #
-#          if (!exists $idA__idB_distance{$id1}) {
-#             $idA__idB_distance{$id1} = {$id2 => $d_ij};
-#          } else {
-#             $idA__idB_distance{$id1}->{$id2} = $d_ij;
-#          }
-#          if (!exists $idA__idB_distance{$id2}) {
-#             $idA__idB_distance{$id2} = {$id1 => $d_ij};
-#          } else {
-#             $idA__idB_distance{$id2}->{$id1} = $d_ij;
-#          }
-#       }
-#    }
-#    return \%idA__idB_distance;
-# }
-
-# sub prune_to_n_closest{
-#    my $idA__idB_dist = shift;
-#    my $n_keep = shift;
-
-# }
-
-
-# sub distance{
-#    my $seq1 = shift;
-#    my $seq2 = shift;
-
-#    die "Sequence lengths are different - bye.\n" if(length $seq1 != length $seq2);
-
-#    my $distance = 0;
-#    my $count_both = 0; # count of snps with data present in both sequences
-#    my $count_missing = 0; # count of snps with data absent in one or both sequences
-#    for (my $i=0; $i < length $seq1; $i++) {
-#       my ($c1, $c2) = (substr($seq1, $i, 1), substr($seq2, $i, 1));
-#       if ($c1 eq '-'  or $c2 eq '-') {
-#          $count_missing++;
-#       } else {
-#          $count_both++;
-#          if ($c2 != $c1) {
-#             if ($c1 == 0) {
-#                $distance += $c2 - $c1;
-#             } elsif ($c1 == 1) {
-#                $distance += 1;
-#             } elsif ($c1 == 2) {
-#                $distance += $c1 - $c2;
-#             } else {
-#                die "c1 has unhandled value: $c1 \n";
-#             }
-#          }
-#       }
-#    }
-#    return ($distance, $count_both);
-# }
